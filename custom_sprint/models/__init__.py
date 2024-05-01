a7fc40917d5607f31707413c1876a8642f7b94d0
Vishal <vishalpundkar123@gmail.com>
2024-05-01 23:48:58 +0530

    xyz1

--- custom_sprint/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import cohorts_SKU
-from . import cohorts_fees_SKU
-from . import cohorts_cohorts
-from . import student_academic
-from . import student_employer
-from . import res_partner
-from . import crm_lead
